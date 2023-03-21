bb90463608d429037db7ef3a886a4779e2f9a19d
Sebastián Dosman <jsdosman0@gmail.com>
2023-03-21 18:32:52 -0500

    Update cors bug

--- src/main/java/co/edu/usbcali/airlinesapp/controllers/UsuarioController.java
+++ src/main/java/co/edu/usbcali/airlinesapp/controllers/UsuarioController.java
@@ -15,8 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/usuario")
 @Slf4j
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
-
+@CrossOrigin(origins = "*", methods= { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
